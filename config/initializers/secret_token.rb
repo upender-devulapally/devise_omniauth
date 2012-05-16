88d905f57d2234dc120f2f5b37c42336817859e5
Upender Devulaplly <upender.devulapally@gmail.com>
2012-05-16 17:02:10 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseOmniauth::Application.config.secret_token = 'dda6f469fd539ad9a6c3f8ad2c38c9a5bef974470a4a39b28f4179d6bd817b4cbff59a01c1286789e00179aa53e97238c5bd4db8ac477da09d556a79f40a17a3'
